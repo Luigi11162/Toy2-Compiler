9d86cf0ce77df546c73abe780a2a71fa0e22e6ab
Luigi11162 <luigibacco01@gmail.com>
2024-01-24 13:02:04 +0100

    Added Error Test Cases

--- source_files/generated_c/out_c.c
+++ source_files/generated_c/out_c.c
@@ -35,74 +35,36 @@ char* result=malloc(sizeof(char)*MAXCHAR);
 result=strcat(result,str1);
 result=strcat(result,str2);
 return result;}
-char* stampa (char* messaggio);
-void sommac (int a, int d, double b, char* *size, double *result);
+void contaspazi ();
 
-int c = 1;
 
-char* stampa (char* messaggio) {
-int i = 0;
-while(i<4)  {
-i = i+1;
- printf("%s",  "\n");
-}
- printf("%s ", messaggio);
-return "ok";
-}
 
-void sommac (int a, int d, double b, char* *size, double *result) {
-*result = a+b+c+d;
-if (*result>100) {
-char* valore = malloc(MAXCHAR);
-valore = strncpy(valore,"grande", MAXCHAR);
-*size = valore;
-}
-else if (*result>50)  {
-char* valore = malloc(MAXCHAR);
-valore = strncpy(valore,"media", MAXCHAR);
-*size = valore;
+void contaspazi () {
+int spazi = 0;
+int altri = 0;
+char* c = malloc(MAXCHAR);
+fflush(stdin);
+scanf("%s", c);
+while(strncmp(c, "EOF", MAXCHAR) != 0 )  {
+if ((strncmp(c, " ", MAXCHAR) == 0)) {
+spazi = spazi+1;
 }
 else
  {
-char* valore = malloc(MAXCHAR);
-valore = strncpy(valore,"piccola", MAXCHAR);
-*size = valore;
+altri = altri+1;
+}
+fflush(stdin);
+scanf("%s", c);
+}
+if ((spazi>altri)) {
+ printf("%s %s", "Piu\' spazi", "\n");
+}
+else
+ {
+ printf("%s %s", "Piu\' altri caratteri", "\n");
 }
 }
 void main () {
-int a = 1;
-double b = 2.2;
-int x = 3;
-char* taglia = malloc(MAXCHAR);
-char* ans1 = malloc(MAXCHAR);
-char* ans = malloc(MAXCHAR);
-ans = strncpy(ans,"no", MAXCHAR);
-double risultato = 0.0;
-char* valore = malloc(MAXCHAR);
-valore = strncpy(valore,"nok", MAXCHAR);
-sommac(a, x, b, &taglia, &risultato);
-valore = stampa(str_concat(str_concat(str_concat(str_concat(str_concat(str_concat(str_concat("la somma di ", integer_to_str(a)), " e "), real_to_str(b)), " incrementata di "), integer_to_str(c)), " è "), taglia));
-valore = stampa(str_concat("ed è pari a ", real_to_str(risultato)));
-printf("vuoi continuare? (si/no) - inserisci due volte la risposta");
-fflush(stdin);
-scanf("%s", ans);
-fflush(stdin);
-scanf("%s", ans1);
-while(strncmp(ans, "si", MAXCHAR) == 0)  {
-printf("inserisci un intero:");
-fflush(stdin);
-scanf("%d", &a);
-printf("inserisci un reale:");
-fflush(stdin);
-scanf("%lf", &b);
-sommac(a, x, b, &taglia, &risultato);
-valore = stampa(str_concat(str_concat(str_concat(str_concat(str_concat(str_concat(str_concat("la somma di ", integer_to_str(a)), " e "), real_to_str(b)), " incrementata di "), integer_to_str(c)), " è "), taglia));
-valore = stampa(str_concat(" ed è pari a ", real_to_str(risultato)));
-printf("vuoi continuare? (si/no):\t");
-fflush(stdin);
-scanf("%s", ans);
-}
- printf("%s",  "\n");
- printf("%s ", "ciao");
+contaspazi();
 }
 
